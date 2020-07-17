15ebe9c08b8ac0a217c2b2e0feef7bf578007af0
jeffwgates <jeff.gates0507@gmail.com>
2020-07-16 20:00:41 -0600

    first commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://exxqadfxlgzanx:30c0270c0c2c28c62b51524fbacd51d0a8f1e8ce1e1a50ddbd2ce13c14a04985@ec2-18-214-119-135.compute-1.amazonaws.com:5432/d5fhhq1fhprqfc"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://pcscbtvlgotbgr:50ddfceac3807eeb54429acb0afff0f27fb56d75fd16e8dc7430dc015c9bd24c@ec2-34-193-117-204.compute-1.amazonaws.com:5432/ddqn9iuu1258id"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -17,59 +17,24 @@ heroku = Heroku(app)
 CORS(app)
 bcrypt = Bcrypt(app)
 
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    admin = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(), nullable=False)
-      
-
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+  
     def __init__(self, username, password):
-      self.admin = username
+      self.username = username
       self.password = password
-
-
+        
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "admin", "password")
-
+        fields = ("id", "username", "password")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-@app.route("/admin/create", methods=["POST"])
-def create_admin():
-    if request.content_type != "application/json":
-        return jsonify("Error: Data must be sent as JSON")
-
-    post_data = request.get_json()
-    admin = post_data.get("admin")
-    password = post_data.get("password")
-    
-    if username_check is not None:
-      return jsonify("Admin Taken")
-
-    hashed_password = bcrypt.generate_password_hash(password).decode("utf8")
-
-    record = User(username, hashed_password)
-    db.session.add(record)
-    db.session.commit()
-
-    return jsonify("Admin Created Successfully")
-
-    @app.route("/admin/get", methods=["GET"])
-def get_all_admin():
-    all_admin = db.session.query(User).all()
-    return jsonify(users_schema.dump(all_users))
-
-@app.route("/admin/get/<id>", methods=["GET"])
-def get_dmain_by_id(id):
-    user = db.session.query(User).filter(User.id == id).first()
-    return jsonify(user_schema.dump(user))
-
-@app.route("/admin/verification", methods=["POST"])
-def verify_admin():
+@app.route("/user/create", methods=["POST"])
+def create_user():
     if request.content_type != "application/json":
         return jsonify("Error: Data must be sent as JSON")
 
@@ -77,19 +42,40 @@ def verify_admin():
     username = post_data.get("username")
     password = post_data.get("password")
 
-    stored_password = db.session.query(User.password).filter(Admin.admin == admin).first()
+    username_check = db.session.query(User.username).filter(User.username == username).first()
+    user_id = db.session.query(User.id).filter(User.username == username).first()
+    if username_check is not None:
+      return jsonify("Username Taken")
+    else:
+      return jsonify("Successful")
 
-    if stored_password is None:
-        return jsonify("User NOT Verified")
+@app.route("/user/get", methods=["GET"])
+def get_all_users():
+    all_users = db.session.query(User).all()
+    return jsonify(users_schema.dump(all_users))
 
-    valid_password_check = bcrypt.check_password_hash(stored_password[0], password)
+@app.route("/user/get/<id>", methods=["GET"])
+def get_user_by_id(id):
+    user = db.session.query(User).filter(User.id == id).first()
+    return jsonify(user_schema.dump(user))
 
-    if valid_password_check == False:
-        return jsonify("Admin NOT Verified")
+# @app.route("/user/verification", methods=["POST"])
+# def verify_user():
+#     if request.content_type != "application/json":
+#         return jsonify("Successful")
+       
 
-    return jsonify("admin Verified")
+#     post_data = request.get_json()
+#     username = post_data.get("username")
+#     password = post_data.get("password")
+    
+    # if stored_password is None:
+    #     return jsonify("User NOT Verified")
 
+    # if valid_password_check == False:
+    #     return jsonify("User NOT Verified")
 
+    # return jsonify("User Verified")
 
 if __name__ == "__main__":
     app.run(debug=True)
